56ac295b21407a9aa34b0c2b9cbab38b97ff58fc
msimbo-coding <SAlmonte@MSIMBO.org>
2021-09-17 23:15:09 -0400

    update

--- src/components/Grid/Grid.styles.js
+++ src/components/Grid/Grid.styles.js
@@ -1,5 +1,19 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+    max-width: var(--maxWidth);
+    margin: 0 auto;
+    padding: 0 20px;
 
-export const Content = styled.div``
+    h1 {
+        color: var(--medGrey);
+
+        @media screen and (max-width: 768px) {
+            font-size: var(--fontBig);
+        }
+    }
+`;
+
+export const Content = styled.div`
+
+`;
