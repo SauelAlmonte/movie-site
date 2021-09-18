6147ba729fc9592040d9931755afe14baa867be7
msimbo-coding <SAlmonte@MSIMBO.org>
2021-09-17 23:07:28 -0400

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
-
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config';
 // Components
+import HeroImage from './HeroImage';
+import Grid from './Grid';
 
 // Hook
 import {useHomeFetch} from '../hooks/useHomeFetch';
@@ -14,7 +15,22 @@ const Home = () => {
 
     console.log(state);
 
-    return <div>Home Page</div>;
+    return (
+        <>
+            {state.results[0] ? (
+                <HeroImage 
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                    title={state.results[0].original_title}
+                    text={state.results[0].overview}
+                /> 
+            ) : null }
+            <Grid header='Popular Movies'>
+                {state.results.map(movie => (
+                    <div key={movie.id}>{movie.title}</div>
+                ))}
+            </Grid>
+        </>
+    );
 };
 
 export default Home;
