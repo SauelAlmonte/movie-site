91f3f15a73c4bea0c252243cd216ee4cf6bfafbd
msimbo-coding <SAlmonte@MSIMBO.org>
2021-09-22 18:50:17 -0400

    update

--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -1,41 +1,53 @@
 import { useState, useEffect } from "react";
 import API from "../API";
+// Helpers
+import { isPersistedState } from "../helpers";
 
 export const useMovieFetch = (movieId) => {
   const [state, setState] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
-    
-  const fetchMovie = async () => {
-    try {
-      setLoading(true);
-      setError(false);
-
-      const movie = await API.fetchMovie(movieId);
-      const credits = await API.fetchCredits(movieId);
-      // Get directos only
-      const directors = credits.crew.filter(
-        (memebr) => memebr.job === "Director"
-      );
-
-      setState({
-        ...movie,
-        actors: credits.cast,
-        directors,
-      });
-
-      setLoading(false);
-    } catch (error) {
-      setError(true);
-    }
-  };
-    
   useEffect(() => {
-    
+    const fetchMovie = async () => {
+      try {
+        setLoading(true);
+        setError(false);
+
+        const movie = await API.fetchMovie(movieId);
+        const credits = await API.fetchCredits(movieId);
+        // Get directos only
+        const directors = credits.crew.filter(
+          (memebr) => memebr.job === "Director"
+        );
+
+        setState({
+          ...movie,
+          actors: credits.cast,
+          directors,
+        });
+
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+      }
+    };
+
+    const sessionState = isPersistedState(movieId);
+
+    if (sessionState) {
+      setState(sessionState);
+      setLoading(false);
+      return;
+    }
 
     fetchMovie();
-  }, [movieId,]);
+  }, [movieId]);
+
+  // Write to sessionStatorage
+  useEffect(() => {
+    sessionStorage.setItem(movieId, JSON.stringify(state));
+  }, [movieId, state])
 
   return { state, loading, error };
 };
