538d5e14cd637c9d0e5f016164b3663f5bb0baa1
msimbo-coding <SAlmonte@MSIMBO.org>
2021-09-22 17:58:22 -0400

    update

--- src/helpers.js
+++ src/helpers.js
@@ -13,3 +13,8 @@ export const calcTime = time => {
     });
     return formatter.format(money);
   };
+
+export const isPersistedState = stateName => {
+  const sessionState = sessionStorage.getItem(stateName);
+  return sessionState && JSON.parse(sessionState);
+  }
--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -1,6 +1,8 @@
 import { useState, useEffect, useRef } from "react";
 // API
 import API from "../API";
+// Helpers
+import { isPersistedState } from "../helpers";
 
 const initialState = {
   page: 0,
@@ -36,6 +38,16 @@ export const useHomeFetch = () => {
 
   // Initial and Search
   useEffect(() => {
+    if (!searchTerm) {
+      const sessionState = isPersistedState('homeState');
+
+      if (sessionState) {
+        console.log('Grabbing from sessionStorage')
+        setState(sessionState);
+        return;
+      }
+    }
+    console.log('Grabbing from API')
     setState(initialState);
     fetchMovies(1, searchTerm);
   }, [searchTerm]);
@@ -47,6 +59,11 @@ export const useHomeFetch = () => {
     fetchMovies(state.page + 1, searchTerm);
     setIsLoadingMore(false);
   }, [isLoadingMore, searchTerm, state.page]);
+
+  // Write to sessionStorage
+  useEffect(() => {
+    if (!searchTerm) sessionStorage.setItem('homeState', JSON.stringify(state))
+  }, [searchTerm, state])
 
   return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore };
 };
